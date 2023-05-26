ab386b2f228e7fe310425ad2534e32be984002c4
manish785 <kumarachilish1997@gmail.com>
2023-05-26 15:16:52 +0530

    refactored the code

--- index.js
+++ index.js
@@ -15,13 +15,14 @@ app.use(express.static(__dirname + '/public'));
 
 //set the view engine
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.json());
-
 // It is used to configure the Express.js application to use the body-parser middleware for parsing
 // URL-encoded data in the request body.
+app.use(bodyParser.urlencoded({extended: true}));
+// It middleware is responsible for parsing incoming requests with JSON payloads. 
+//and is commonly used to handle HTTP POST requests where the data being sent to the server is in JSON format.
+app.use(express.json());
 
-
+// here, connect to the database 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>console.log("DB connection Succesful"))
@@ -30,7 +31,7 @@ mongoose.connect(process.env.MONGO_URL)
 });
 
 
-//get api
+//get api, will fetch all the tasks
 app.get('/', async function(req, res){
     try{
         let todoList = await Todo.find({});
@@ -41,27 +42,25 @@ app.get('/', async function(req, res){
     }
 })
 
-//post api 
-//route for adding new task
-app.post("/newtodo", async function(req, res) {
+
+//post api, route for adding new task 
+app.post('/newtodo', async function(req, res) {
     var newTask = new Todo({
       name: req.body.name
     });
   
     try {
-      console.log(newTask);
       await Todo.create(newTask);
-      console.log('Todo List created successfully');
-      return res.status(200).json({ message: 'Todo List created successfully' });
+      return res.status(200).json({ message: 'Todo List created successfully'});
     } catch (err) {
       console.log('Error while creating the todo List', err);
-      return res.status(500).json({ message: 'Error while creating the todo List' });
+      return res.status(500).json({ message: 'Error while creating the todo List'});
     }
   });
 
- // route to update a task by id
- // PATCH API
-  app.patch("/update/:id", async (req, res) => {
+
+ // PATCH API, route to update the particular task by id
+  app.patch('/update/:id', async (req, res) => {
     let taskId = req.params.id; // Get the id from the API
     let updatedTask = req.body.name; // Get the updated task from the request body
  
@@ -75,11 +74,10 @@ app.post("/newtodo", async function(req, res) {
     }
   });
   
-//route to delete a task by id
-//DELETE API
-app.delete("/delete/:id", async (req, res)=>{
+
+//DELETE API, route to delete the particular task by id
+app.delete('/delete/:id', async (req, res)=>{
     let taskId = req.params.id;//get the id from the api 
-    console.log(req.params.id);
     try{
         await mongoose.model('Todo').deleteOne({_id: taskId});
         console.log("Task successfully deleted from database");
@@ -90,9 +88,9 @@ app.delete("/delete/:id", async (req, res)=>{
     }
 });
 
-//route for deleting all tasks
-// DELETE ALL TASKS API
-app.get("/delAlltodo", async (req, res)=>{
+
+//DELETE ALL TASKS API, route for deleting all tasks
+app.delete('/delAlltodo', async (req, res)=>{
     try{
         await mongoose.model('Todo').deleteMany({});
         console.log(`Deleted all tasks`);
@@ -103,6 +101,7 @@ app.get("/delAlltodo", async (req, res)=>{
     }
 });
 
+// here, server is running on the specific port number
 app.listen(port, function(err){
     if(err){
         console.log('Error while running the server', err);
